63b4ca324eec1ba2a370a1ad15144012c26564c8
Lev Tsypin <lev@thinkshout.com>
2014-06-27 12:58:59 -0700

    Removing email functionality.

--- jekyll-hook.js
+++ jekyll-hook.js
@@ -7,8 +7,6 @@ var app     = express();
 var queue   = require('queue-async');
 var tasks   = queue(1);
 var spawn   = require('child_process').spawn;
-var email   = require('emailjs/email');
-var mailer  = email.server.connect(config.email);
 
 app.use(express.bodyParser());
 
@@ -38,7 +36,7 @@ app.post('/hooks/jekyll', function(req, res) {
 
         // End early if not permitted branch
         if (config.branches.indexOf(data.branch) === -1) {
-            console.log(data.branch ' is not configured for deployment.');
+            console.log(data.branch + ' is not configured for deployment.');
             if (typeof cb === 'function') cb();
             return;
         }
@@ -55,7 +53,6 @@ app.post('/hooks/jekyll', function(req, res) {
         run(config.scripts.build, params, function(err) {
             if (err) {
                 console.log('Failed to build: ' + data.owner + '/' + data.repo);
-                send('Your website at ' + data.owner + '/' + data.repo + ' failed to build.', 'Error building site', data);
 
                 if (typeof cb === 'function') cb();
                 return;
@@ -65,7 +62,6 @@ app.post('/hooks/jekyll', function(req, res) {
             run(config.scripts.publish, params, function(err) {
                 if (err) {
                     console.log('Failed to publish: ' + data.owner + '/' + data.repo);
-                    send('Your website at ' + data.owner + '/' + data.repo + ' failed to publish.', 'Error publishing site', data);
 
                     if (typeof cb === 'function') cb();
                     return;
@@ -73,7 +69,6 @@ app.post('/hooks/jekyll', function(req, res) {
 
                 // Done running scripts
                 console.log('Successfully rendered: ' + data.owner + '/' + data.repo);
-                send('Your website at ' + data.owner + '/' + data.repo + ' was succesfully published.', 'Succesfully published site', data);
 
                 if (typeof cb === 'function') cb();
                 return;
@@ -102,16 +97,4 @@ function run(file, params, cb) {
     process.on('exit', function (code) {
         if (typeof cb === 'function') cb(code !== 0);
     });
-}
-
-function send(body, subject, data) {
-    if (config.email && data.pusher.email) {
-        var message = {
-            text: body,
-            from: config.email.user,
-            to: data.pusher.email,
-            subject: subject
-        };
-        mailer.send(message, function(err) { if (err) console.warn(err); });
-    }
 }
